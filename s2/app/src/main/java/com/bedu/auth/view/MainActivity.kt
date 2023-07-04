5dbfb0b74af1a33b00f2c4bc1721be53ee8571bf
Andrés Ramírez <andres200493@gmail.com>
2023-07-03 21:07:01 -0600

    S2 -> E3

--- s2/app/src/main/java/com/bedu/auth/view/MainActivity.kt
+++ s2/app/src/main/java/com/bedu/auth/view/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Toast
 import com.bedu.auth.databinding.ActivityOptionsBinding
 import com.google.firebase.FirebaseApp
+import com.google.firebase.crashlytics.CustomKeysAndValues
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 class MainActivity : Activity() {
 
@@ -42,6 +44,27 @@ class MainActivity : Activity() {
         binding.btnCrash.setOnClickListener {
             Toast.makeText(this, "Crash!", Toast.LENGTH_SHORT).show()
             throw RuntimeException("Test Crash") // Force a crash
+        }
+        binding.btnInfo.setOnClickListener {
+            Toast.makeText(this, "setInfo!", Toast.LENGTH_SHORT).show()
+            FirebaseCrashlytics.getInstance().setCustomKey("str_key", "Bedu")
+            FirebaseCrashlytics.getInstance().setCustomKey("bool_key", true)
+            FirebaseCrashlytics.getInstance().setCustomKey("int_key", 15)
+            FirebaseCrashlytics.getInstance().setCustomKey("long_key", 15L)
+            FirebaseCrashlytics.getInstance().setCustomKey("float_key", 1.0f)
+            FirebaseCrashlytics.getInstance().setCustomKey("double_key", 1.0)
+
+            FirebaseCrashlytics.getInstance().setCustomKeys(CustomKeysAndValues.Builder()
+                .putString("str_key", "Beto")
+                .putInt("int_key_1", 16)
+                .build())
+
+            FirebaseCrashlytics.getInstance().log("Estoy dentro del btnInfo")
+            FirebaseCrashlytics.getInstance().log("Higgs-Boson detected! Bailing out")
+
+            FirebaseCrashlytics.getInstance().setUserId("123")
+
+//            throw RuntimeException("Test Crash") // Force a crash
         }
     }
 
