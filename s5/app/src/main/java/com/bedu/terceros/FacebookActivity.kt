0c994e9e2794dae5c8ecfb5eb40ddf601ee62e0d
Andrés Ramírez <andres200493@gmail.com>
2023-07-12 19:57:34 -0600

    s5 -> e1

--- s5/app/src/main/java/com/bedu/terceros/FacebookActivity.kt
+++ s5/app/src/main/java/com/bedu/terceros/FacebookActivity.kt
@@ -1,8 +1,11 @@
 package com.bedu.terceros
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bedu.terceros.databinding.ActivityFacebookBinding
+import com.facebook.share.model.ShareLinkContent
+import com.facebook.share.widget.ShareDialog
 
 
 class FacebookActivity : AppCompatActivity() {
@@ -15,6 +18,14 @@ class FacebookActivity : AppCompatActivity() {
         binding = ActivityFacebookBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.btnLink.setOnClickListener {
+            val content = ShareLinkContent.Builder()
+                .setContentUrl(Uri.parse("https://developers.facebook.com/"))
+                .build()
+
+            ShareDialog.show(this, content)
+        }
 
     }
 }
