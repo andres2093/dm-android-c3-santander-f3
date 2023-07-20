b8086893429b176bc9f9de21fa0149fd29374042
Andrés Ramírez <andres200493@gmail.com>
2023-07-19 21:14:59 -0600

    S7 -> E3

--- s7/app/src/main/java/com/bedu/librerias/ShortbreadActivity.kt
+++ s7/app/src/main/java/com/bedu/librerias/ShortbreadActivity.kt
@@ -2,8 +2,11 @@ package com.bedu.librerias
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.pm.ShortcutManagerCompat.ShortcutMatchFlags
 import com.bedu.librerias.databinding.ActivityShortbreadBinding
+import shortbread.Shortcut
 
+@Shortcut(id = "shortBread", icon = R.drawable.ic_android, shortLabel = "ShortBread")
 class ShortbreadActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityShortbreadBinding
